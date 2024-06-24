4dbfb77c339e3e1cebfaa8e6b55107ce107f5797
amwill44 <116133323+amwill44@users.noreply.github.com>
2024-06-24 13:33:47 -0700

    lesson 1.2
    update code to start the pokedex, and add the first few pokemon.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
-alert('Hello World');
-let favoriteFood = 'Mac n Cheese';
-document.write(favoriteFood);
+let unit = 'cm';
+
+let pokemonList = [
+    { name: 'Bulbasaur', height: 70 + unit, type: ['grass', 'poison']},
+    { name: 'Charmander', height: 60 + unit, type: ['fire']},
+    { name: 'Squirtle', height: 50 + unit, type: ['water']}
+];
