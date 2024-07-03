6b48ea7ab99272c8d96c269a9b5e4d5b67a58cbf
amwill44 <116133323+amwill44@users.noreply.github.com>
2024-07-03 14:25:38 -0500

    update 1.2
    adjusted the unit code to keep the height a numerical value.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 let unit = 'cm';
 
 let pokemonList = [
-    { name: 'Bulbasaur', height: 70 + unit, type: ['grass', 'poison']},
-    { name: 'Charmander', height: 60 + unit, type: ['fire']},
-    { name: 'Squirtle', height: 50 + unit, type: ['water']}
+    { name: 'Bulbasaur', height: 70, unit: unit, type: ['grass', 'poison']},
+    { name: 'Charmander', height: 60, unit: unit, type: ['fire']},
+    { name: 'Squirtle', height: 50, unit: unit, type: ['water']}
 ];
