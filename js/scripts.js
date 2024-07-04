cac0240d0204a51fca20af6d841cfa8ab67dfbbe
amwill44 <116133323+ashwillcode@users.noreply.github.com>
2024-07-03 20:23:41 -0500

    1.3
    Added a for loop that displays in the DOM

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,16 @@ let unit = 'cm';
 let pokemonList = [
     { name: 'Bulbasaur', height: 70, unit: unit, type: ['grass', 'poison']},
     { name: 'Charmander', height: 60, unit: unit, type: ['fire']},
-    { name: 'Squirtle', height: 50, unit: unit, type: ['water']}
+    { name: 'Squirtle', height: 50, unit: unit, type: ['water']},
+    { name: 'Pikachu', height: 40, unit: unit, type: ['electric']}
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+    let pokemon = pokemonList[i];
+    let output = pokemon.name + ' (height: ' + pokemon.height + unit + ')'; 
+
+    if (pokemon.height > 65) {
+        output += ' - Wow, that\'s big!';
+    }
+    document.write(output + '<br>');
+}
