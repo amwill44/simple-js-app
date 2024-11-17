ce514d533c4802d7bcc91050aa3b6cf4313049b7
amwill44 <116133323+ashwillcode@users.noreply.github.com>
2024-11-16 18:09:15 -0700

    added a IIFE around pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,35 @@
-let unit = 'cm';
 
-let pokemonList = [
-    { name: 'Bulbasaur', height: 70, unit: unit, type: ['grass', 'poison']},
-    { name: 'Charmander', height: 60, unit: unit, type: ['fire']},
-    { name: 'Squirtle', height: 50, unit: unit, type: ['water']},
-    { name: 'Pikachu', height: 40, unit: unit, type: ['electric']}
-];
 
-pokemonList.forEach(function(pokemon) {
+let pokemonRepository = (function () {
+
+    let unit = 'cm';
+
+    let pokemonList = [
+        { name: 'Bulbasaur', height: 70, unit: unit, type: ['grass', 'poison']},
+        { name: 'Charmander', height: 60, unit: unit, type: ['fire']},
+        { name: 'Squirtle', height: 50, unit: unit, type: ['water']},
+        { name: 'Pikachu', height: 40, unit: unit, type: ['electric']}
+    ];
+
+    return {
+        getAll: function () {
+            return pokemonList;
+        }, 
+        add: function (pokemon) {
+            if (typeof pokemon === 'object' &&
+                'name' in pokemon &&
+                'height' in pokemon &&
+                'type' in pokemon) {
+                pokemonList.push(pokemon);
+                } 
+            else {
+                console.error('Invalid Pokemon object');
+            }
+        }
+    };
+})();
+
+pokemonRepository.getAll().forEach(function(pokemon) {
     let output = pokemon.name + ' (height: ' + pokemon.height + unit + ')'; 
 
     if (pokemon.height > 65) {
@@ -15,3 +37,5 @@ pokemonList.forEach(function(pokemon) {
     }
     document.write(output + '<br>');
 }); 
+
+pokemonRepository.add({name: 'Mew', height: 40, unit: 'cm', type ['psychic']});
